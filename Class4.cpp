619b3f776ebc5e7f7c7707a31f7da123efa4047a
bfknisely <35206584+bfknisely@users.noreply.github.com>
2018-08-30 12:13:06 -0400

    initialize class 4

---
+++ Class4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using napespace std;
+
+void main() {
+	int iNumber1;
+	cout << "Enter a number between 0-10: " << endl;
+	cin >> iNumber1;
+	
+	for (int i=0; i < iNumber; i++) {
+		cout << i
+	}
+}
