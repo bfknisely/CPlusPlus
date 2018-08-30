4e87cb69cf30020253a458a0c82786d73f0a7fc5
bfknisely <35206584+bfknisely@users.noreply.github.com>
2018-08-30 12:20:32 -0400

    added continue statements

--- Class4.cpp
+++ Class4.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
-using napespace std;
+using namespace std;
 
-void main() {
-	int iNumber1;
-	cout << "Enter a number between 0-10: " << endl;
-	cin >> iNumber1;
-	
-	for (int i=0; i < iNumber; i++) {
-		cout << i
+int main() {
+	// int iNumber1;
+	// cout << "Enter a number between 0-10: " << endl;
+	// cin >> iNumber1;
+	int i;
+	for (i=0; i < 10; i++) {
+		if (i == 5){
+			continue;
+		}
+		cout << i << ',';
 	}
+	cout << "\n Loop ended at " << i << endl;
+	return 0;
 }
